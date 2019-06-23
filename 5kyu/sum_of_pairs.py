1cc978f0417046598c17e6535a7a314a1ca2defd
thevalenciandev <thevalenciandev@gmail.com>
2019-06-23 14:10:04 +0100

    5 kyu: https://www.codewars.com/kata/sum-of-pairs/train/python Second solution, trying to optimise...

--- 5kyu/sum_of_pairs.py
+++ 5kyu/sum_of_pairs.py
@@ -1,21 +1,35 @@
+ints = [1, 4, 8, 7, 3, 15]
+# l2= [1, -2, 3, 0, -6, 1]
+# l3= [20, -13, 40]
+# l4= [1, 2, 3, 4, 1, 0]
+# ints= [10, 5, 2, 3, 7, 5]
+# l6= [4, -2, 3, 3, 4]
+# l7= [0, 2, 0]
+# l8= [5, 9, 13, -3]
+sum = 10
+
+
 def sum_pairs(ints, sum):
-    left_index = None
-    right_index = None
+    left_index = len(ints)
+    right_index = len(ints)
     found_sum = False
-    for i in range(len(ints)):
-        for j in range(i + 1, len(ints)):
-            if found_sum and j > right_index:
-                break
-            if found_sum and i >= right_index:  # don't keep looking, we already got the best solution
-                return [ints[left_index], ints[right_index]]
+    i = 0
+    j = 1
+    while i < right_index:
+        while j < right_index:
             if ints[i] + ints[j] == sum:
-                if not found_sum or j < right_index:
+                if j < right_index:
                     left_index = i
                     right_index = j
                     found_sum = True
-                    break  # don't keep looking on this line
+            j += 1
+        i += 1
+        j = i + 1
 
     if found_sum:
         return [ints[left_index], ints[right_index]]
     else:
         return None
+
+
+print(sum_pairs(ints, sum))
