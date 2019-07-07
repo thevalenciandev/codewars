890d5b702ed7523fad2aad3096224c91353e13a7
thevalenciandev <thevalenciandev@gmail.com>
2019-07-07 14:25:15 +0100

    Adding more coverage for red cards test

--- 6kyu/men_still_standing.py
+++ 6kyu/men_still_standing.py
@@ -40,5 +40,7 @@ def test_two_yellow_cards_for_same_player_in_same_team(cards, expected_result):
     assert men_still_standing(cards) == expected_result
 
 
-def test_red_cards():
-    assert men_still_standing(['A11R']) == (10, 11)
+@pytest.mark.parametrize('cards, expected_result', [(['A11R'], (10, 11)),
+                                                    (['B11R'], (11, 10))])
+def test_red_cards(cards, expected_result):
+    assert men_still_standing(cards) == expected_result
